b86b37d02d10eb154886d20e9088a958c07d3283
Jani-kahkonen <h9575@student.jamk.fi>
2016-01-24 02:41:02 +0200

    Tehtava 2 valmis

---
+++ IIO11300Vktehtavat/Tehtava2/BLLotto.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tehtava2
+{
+    public class Lotto
+    {
+        private string numbers;
+        private int lenght, max;
+        private bool ext;
+
+        public bool Ext
+        {
+            get { return ext; }
+            set { ext = value; }
+        }
+
+        public int Lenght
+        {
+            get { return lenght; }
+            set { lenght = value; }
+        }
+
+        public int Max
+        {
+            get { return max; }
+            set { max = value; }
+        }
+
+        public string Numbers
+        {
+            get { return numbers; }
+            set { numbers = value; }
+        }
+
+        public string CreateLotto()
+        {
+            int[] arr = new int[lenght];
+
+            random(arr);
+
+            bubbleSort(arr);
+
+            for (int i = 0; i != arr.Length; i++)
+            {
+                numbers += " " + Convert.ToString(arr[i]);
+            }
+
+            if (ext == true)
+            {
+                lenght = 2;
+                ext = false;
+
+                numbers += " -";
+                this.CreateLotto();
+            }
+
+            return numbers += "\n";
+        }
+
+        public void random(int[] arr)
+        {
+            Random rnd = new Random();
+
+            for (int i = 0; i < lenght; ++i)
+            {
+                arr[i] = rnd.Next(1, max);
+            }
+        }
+
+        public void bubbleSort(int[] arr)
+        {
+            bool didSwap;
+            do
+            {
+                didSwap = false;
+                for (int i = 0; i < arr.Length - 1; i++)
+                {
+                    if (arr[i] > arr[i + 1])
+                    {
+                        int temp = arr[i + 1];
+                        arr[i + 1] = arr[i];
+                        arr[i] = temp;
+                        didSwap = true;
+                    }
+                }
+            } while (didSwap);
+        }
+    }
+}
---
+++ IIO11300Vktehtavat/Tehtava2/MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Tehtava2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.comboBox.Items.Add("Suomi-lotto");
+            this.comboBox.Items.Add("Viking-lotto"); 
+            this.comboBox.Items.Add("Eurojackpot-lotto");
+            this.comboBox.SelectedIndex = 0;
+        }
+
+        private void bttClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void bttDraw_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Tehtava2.Lotto lotto = new Tehtava2.Lotto();
+
+                // Arvottavien rivien lukumäärä.
+                int count = int.Parse(txtCount.Text);
+
+                // Pelin valinta
+
+                if (this.comboBox.SelectedIndex == 0)
+                {
+                    lotto.Lenght = 7;
+                    lotto.Max = 39;
+                }
+                else if (this.comboBox.SelectedIndex == 1)
+                {
+                    lotto.Lenght = 6;
+                    lotto.Max = 48;
+                }
+                else if (this.comboBox.SelectedIndex == 2)
+                {
+                    lotto.Lenght = 5;
+                    lotto.Max = 50;
+                    lotto.Ext = true;
+                }
+
+                // Tee rivit
+
+                for(int i = 0; i < count; ++i)
+                {
+                    txtContainer.Text += lotto.CreateLotto();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void bttClear_Click(object sender, RoutedEventArgs e)
+        {
+            txtContainer.Text = String.Empty;
+        }
+    }
+}
