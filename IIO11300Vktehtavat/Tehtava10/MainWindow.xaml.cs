ea80cc1c0f3bc049864c999d4cc267050673afaa
Jani-kahkonen <h9575@student.jamk.fi>
2016-03-29 21:32:31 +0300

    Tehtava 10 kesken.

---
+++ IIO11300Vktehtavat/Tehtava10/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Tehtava10
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public OleDbConnection oleCon;
+        public OleDbCommand oleComd;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            BindData();
+        }
+
+        public void BindData()
+        {
+
+            oleCon = new OleDbConnection(@"Provider = Microsoft.ACE.OLEDB.12.0; Data Source = C:\Users\Jani\Documents\Visual Studio 2015\Projects\IIO11300\IIO11300Vktehtavat\Tehtava10\SMLiiga.accdb");
+
+            oleComd = new OleDbCommand("SELECT * FROM Pelaajat", oleCon);
+
+            DataSet dtst = new DataSet();
+
+            OleDbDataAdapter adpt = new OleDbDataAdapter();
+
+            try
+            {
+                oleCon.Open();
+
+                adpt.SelectCommand = oleComd;
+
+                adpt.Fill(dtst, "Pelaajat");
+
+                lstPlayer.DataContext = dtst;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                oleCon.Close();
+            }
+        }
+
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ IIO11300Vktehtavat/Tehtava10/Pelaaja.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tehtava10
+{
+    class Pelaaja
+    {
+        private String etunimi, sukunimi, seura;
+        private int hinta;
+
+        public string Etunimi
+        {
+            get { return etunimi; }
+            set { etunimi = value; }
+        }
+
+        public string Sukunimi
+        {
+            get { return sukunimi; }
+            set { sukunimi = value; }
+        }
+
+        public int Hinta
+        {
+            get { return hinta; }
+            set { hinta = value; }
+        }
+
+        public string Seura
+        {
+            get { return seura; }
+            set { seura = value; }
+        }
+
+        private string getKokonimi()
+        {
+            return etunimi + " " + sukunimi;
+        }
+
+        public string getEsitysnimi()
+        {
+            return etunimi + " " + sukunimi + ", " + seura;
+        }
+
+        #region CONSTRUCTORS
+        public Pelaaja(string a, string b, int c, string d)
+        {
+            this.etunimi = a;
+            this.sukunimi = b;
+            this.hinta = c;
+            this.seura = d;
+        }
+        #endregion
+    }
+}
